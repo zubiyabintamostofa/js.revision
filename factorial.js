f377c1f134d3e6311a0ba2c1c3c0cbd8cf6da760
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-01-31 21:50:57 +0600

    pain

---
+++ factorial.js
@@ -0,0 +1,25 @@
+// var factorial=1;
+// for(var i=1;i<=7;i++){
+    
+//     factorial=factorial*i;
+// }
+// console.log(factorial);
+
+
+// factorial-function
+/*
+let factorial=1;
+for(let i=1;i<=5;i++){
+    factorial=factorial*i;
+}console.log(factorial);
+*/
+function getFactorial(number){
+    let factorial =1;
+    for (let i=1;i<=number;i++)
+    {
+        factorial=factorial*i;
+    }
+    return factorial;
+}
+var firstFactorial=getFactorial(13);
+console.log('factorial of 9',firstFactorial);
