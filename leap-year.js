05393a594093119056f527a45627b09953c507ef
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-01-31 19:55:30 +0600

    one

--- leap-year.js
+++ leap-year.js
@@ -1,10 +1,12 @@
-function isLeapYear(year)
-{
+ function isLeapYear(year)
+ {
     if(year%4==0){
-        return true;
-    }
-    return false;
-}
-const myYear=2022;
-const myYearLeapYear=isLeapYear (myYear);
-console.log('is my year leap year',myYearLeapYear);
+         return true;
+     }
+     return false;
+ }
+ const myYear=2022;
+ const myYearLeapYear=isLeapYear (myYear);
+ console.log('is my year leap year',myYearLeapYear);
+
+
