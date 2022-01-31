f0d8c3ef6747d8d8cf06250a226f064391e8b882
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-01-31 13:03:48 +0600

    hey

---
+++ leap-year.js
@@ -0,0 +1,10 @@
+function isLeapYear(year)
+{
+    if(year%4==0){
+        return true;
+    }
+    return false;
+}
+const myYear=2022;
+const myYearLeapYear=isLeapYear (myYear);
+console.log('is my year leap year',myYearLeapYear);
