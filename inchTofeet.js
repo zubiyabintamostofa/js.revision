94582a580752f0618c5ea5167b068277d13e6d6a
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-01-31 12:00:34 +0600

    new

---
+++ inchTofeet.js
@@ -0,0 +1,11 @@
+// var mile=132;
+// var kilometer=mile/12;
+// console.log(feet);
+function mileToKilometer(miles){
+    var km=miles*1.60934;
+    return km;
+}
+var marathon =mileToKilometer(26.2);
+console.log('marathon in km:',marathon);
+
+
